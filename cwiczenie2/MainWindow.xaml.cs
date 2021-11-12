c26e94188da757259b79b8341c9131f5374a8f35
Marek Hyra <mhyra@student.scalda.nl>
2021-11-12 09:23:09 +0100

    Add project files.

---
+++ cwiczenie2/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cwiczenie2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            String name = "Quentin";
+            int x = 3;
+            x = x * 17;
+            double d = Math.PI / 2;
+            myLabel.Text = "nazwa to " + name
+                + "\nx jest równe " + x
+                + "\nd jest równe " + d;
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            int x = 5;
+            if (x == 10)
+            {
+                myLabel.Text = "x musi być równe 10";
+            }
+            else
+            {
+                myLabel.Text = "x nie jest równe 10";
+            }
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            int someValue = 4;
+            string name = "Krzysiek";
+            if ((someValue == 3) && (name.Equals("Janek")))
+            {
+                myLabel.Text = "someValue jest równe 3 i name jest równe Janek";
+            }
+            myLabel.Text = "ten wiersz jest wykonywane bez względu na warunki";
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            int count = 0;
+            while (count < 10)
+            {
+                count = count + 1;
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                count = count - 1;
+            }
+            myLabel.Text = "Odpowiedź brzmi " + count;
+        }
+    }
+}
